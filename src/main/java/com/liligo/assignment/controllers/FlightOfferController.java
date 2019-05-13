ced25aacd20e31bf41904251a69efbe3395df5f0
88menyus@gmail.com <88menyus@gmail.com>
2019-05-13 14:49:17 +0200

    Update Controller

--- src/main/java/com/liligo/assignment/controllers/FlightOfferController.java
+++ src/main/java/com/liligo/assignment/controllers/FlightOfferController.java
@@ -4,26 +4,27 @@ import com.liligo.assignment.models.FlightOffer;
 import com.liligo.assignment.services.FlightOfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class FlightOfferController {
 
     @Autowired
     FlightOfferService flightOfferService;
 
-    @RequestMapping("/offers")
+    @GetMapping("/offers")
     public List<FlightOffer> index() {
         return flightOfferService.findAllFlights();
     }
 
-    @RequestMapping("/offers/{numberOfPassengers}")
+    @GetMapping("/offers/{numberOfPassengers}")
     public List<FlightOffer> index(@PathVariable int numberOfPassengers) {
-        return flightOfferService.findOfferByNoOfPassengers(numberOfPassengers);
+        if(numberOfPassengers < 0) {
+            return flightOfferService.findOfferByNoOfPassengers(numberOfPassengers);
+        }
+        return index();
     }
 
     @PostMapping("/save")
