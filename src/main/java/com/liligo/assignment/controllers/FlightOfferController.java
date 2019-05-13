49b35c7f5a50b5a8c962059d34d32aa8fe7356ff
88menyus@gmail.com <88menyus@gmail.com>
2019-05-13 15:12:50 +0200

    Update change method after testing with postman

--- src/main/java/com/liligo/assignment/controllers/FlightOfferController.java
+++ src/main/java/com/liligo/assignment/controllers/FlightOfferController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,8 +29,8 @@ public class FlightOfferController {
     }
 
     @PostMapping("/save")
-    public String save(List<FlightOffer> offers) {
+    public List<FlightOffer> save(ArrayList<FlightOffer> offers) {
         flightOfferService.saveOffers(offers);
-        return "redirect:/offers";
+        return index();
     }
 }
