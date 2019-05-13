0b382641b95713c512c7276db241db763859ca73
88menyus@gmail.com <88menyus@gmail.com>
2019-05-13 20:24:31 +0200

    Create equals and hashCode method for Location

--- src/main/java/com/liligo/assignment/models/Location.java
+++ src/main/java/com/liligo/assignment/models/Location.java
@@ -3,6 +3,8 @@ package com.liligo.assignment.models;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import javax.persistence.*;
 
@@ -18,4 +20,30 @@ public class Location {
     private long id;
     private String country;
     private String city;
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(country)
+                .append(city)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(other == null) {
+            return false;
+        }
+        if(other == this) {
+            return true;
+        }
+        if (other.getClass() != getClass()) {
+            return false;
+        }
+        Location otherLocation = (Location) other;
+        return new EqualsBuilder()
+                .append(country, otherLocation.getCountry())
+                .append(city, otherLocation.getCity())
+                .isEquals();
+    }
 }
