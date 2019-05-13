c8ee1f3addf9bceb3d9695b0d26ee4828a000363
88menyus@gmail.com <88menyus@gmail.com>
2019-05-13 12:43:00 +0200

    Create FlightOfferRepository

---
+++ src/main/java/com/liligo/assignment/repos/FlightOfferRepository.java
@@ -0,0 +1,7 @@
+package com.liligo.assignment.repos;
+
+import com.liligo.assignment.models.FlightOffer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FlightOfferRepository extends CrudRepository<FlightOffer, Long> {
+}
