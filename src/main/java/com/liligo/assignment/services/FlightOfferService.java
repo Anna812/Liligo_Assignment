93b1cce56b156d4e7475ac9ab5c58f9c5f33e83c
88menyus@gmail.com <88menyus@gmail.com>
2019-05-13 12:49:40 +0200

    Create FlightOfferServicefor crud operations on repository

---
+++ src/main/java/com/liligo/assignment/services/FlightOfferService.java
@@ -0,0 +1,23 @@
+package com.liligo.assignment.services;
+
+import com.liligo.assignment.models.FlightOffer;
+import com.liligo.assignment.repos.FlightOfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlightOfferService {
+
+    @Autowired
+    FlightOfferRepository repository;
+
+    public List<FlightOffer> findAllFlights() {
+        return (List<FlightOffer>)repository.findAll();
+    }
+
+    public void saveOffers(List<FlightOffer> offers) {
+        repository.saveAll(offers);
+    }
+}
